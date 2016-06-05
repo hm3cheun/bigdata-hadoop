7c41be5e0546d03bd1c07233c549b0d7dd0671a7
cloudera <cloudera@quickstart.cloudera>
2016-06-05 08:18:04 -0700

    first

---
+++ A2/join2_mapper.py
@@ -0,0 +1,9 @@
+import sys
+
+for line in sys.stdin:
+    tv_show, value = line.split(',')
+    tv_show = tv_show.strip()
+    value = value.strip()
+
+    if value.isdigit() or value == 'ABC':
+        print('{0}\t{1}'.format(tv_show, value))
